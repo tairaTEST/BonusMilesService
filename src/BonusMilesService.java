e511bd3c14cd311620e4664475058f0ad0882b79
Tatiana Noskova <tnoskowa@mail.ru>
2023-03-17 12:21:42 +0500

    2class

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,16 @@
 public class BonusMilesService {
-    public int calculate (int cost) {
+
+    public int calculate(int cost) {
+
         int result;
+        if (cost >= 20)
+        {
+            result = cost / 20;
+        } else {
+            result = 0;
+        }
         return result;
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -6,4 +6,3 @@ public class Main {
         System.out.println(miles);
     }
 }
-}
